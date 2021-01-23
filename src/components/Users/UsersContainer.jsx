5f8cf9ba26613ba74f2e713da3b7802210b180f2
Khmara Dmytro <khmaracoach@gmail.com>
2021-01-24 01:29:30 +0200

    add preloader

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -35,7 +35,7 @@ class UsersContainer extends React.Component {
 
     render() {
         return <>
-            { this.props.isFetching ? <Preloader /> : null}
+            { this.props.isFetching ? <Preloader /> : null }
             <Users totalUsersCount={this.props.totalUsersCount}
                    pageSize={this.props.pageSize}
                    currentPage={this.props.currentPage}
