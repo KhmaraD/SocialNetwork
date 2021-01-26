797cf3a2238e465a0bae87c86ae741537ff83624
Khmara Dmytro <khmaracoach@gmail.com>
2021-01-27 00:33:33 +0200

    refactoring user profile

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -5,6 +5,7 @@ import {connect} from "react-redux";
 import {setUserProfile} from "../../redux/profile-reducer";
 
 class ProfileContainer extends React.Component {
+
     componentDidMount() {
         axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
             .then(response => {
@@ -14,13 +15,13 @@ class ProfileContainer extends React.Component {
 
     render() {
         return (
-            <Profile {...this.props} />
+            <Profile {...this.props} profile={this.props.profile} />
         )
     }
 }
 
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile
-})
+});
 
 export default connect (mapStateToProps, {setUserProfile})(ProfileContainer);
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
+import Preloader from "../../common/Preloader/Preloader";
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+    if (!props.profile) {
+        return <Preloader />
+    }
+
     return (
         <div>
             <img className={s.coverImg}
                  src="https://women4it.eu/wp-content/uploads/2020/02/Canva-html-php-java-source-code-2-scaled-e1581350484417-1600x420.jpg"
                  alt="img"/>
             <div className={s.description}>
+                <img src={props.profile.photos.large} />
                 ava + description
             </div>
         </div>
