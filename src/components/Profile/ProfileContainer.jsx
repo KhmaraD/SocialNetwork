3ed1b7a1af5f95cc93bab37389ea8b97df429d3b
Khmara Dmytro <khmaracoach@gmail.com>
2021-02-01 11:54:18 +0200

    refactoring (thunk)

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import Profile from "./Profile";
-import * as axios from "axios";
 import {connect} from "react-redux";
 import {setUserProfile} from "../../redux/profile-reducer";
 import {withRouter} from "react-router";
+import {usersAPI} from "../api/api";
 
 class ProfileContainer extends React.Component {
 
     componentDidMount() {
-        let userId = this.props.match.params.userId;
-        if (!userId) {
-            userId = 14408;
-        }
-
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
-            .then(response => {
-                this.props.setUserProfile(response.data);
+        usersAPI.getUserProfile(this.props.match.params.userId)
+            .then(data => {
+                this.props.setUserProfile(data);
             });
     }
 
