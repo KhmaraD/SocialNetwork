98a59c220575a0f8df449f32b65bbbb3db26a0d2
Khmara Dmytro <khmaracoach@gmail.com>
2021-02-15 12:00:02 +0200

    gh-pages

--- src/index.js
+++ src/index.js
@@ -5,16 +5,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter >
         <Provider store={store}>
             <App />
         </Provider>
-    </BrowserRouter>, document.getElementById('root'));
+    </HashRouter>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// <BrowserRouter basename={process.env.PUBLIC_URL}>
 reportWebVitals();
