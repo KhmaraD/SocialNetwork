f0547e508c1bff88aa17f35c1418e577facffb92
Khmara Dmytro <khmaracoach@gmail.com>
2021-02-22 09:03:27 +0200

    add captcha

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Field, reduxForm} from "redux-form";
+import {reduxForm} from "redux-form";
 import {createField, Input} from "../common/FormsControls/FormsControls";
 import {required} from "../utils/validators/validators";
 import {login} from "../../redux/auth-reducer";
@@ -9,7 +9,7 @@ import styleForm from "../common/FormsControls/FormsControls.module.css";
 import style from "./Login.module.css"
 
 
-const LoginForm = ({handleSubmit, error}) => {
+const LoginForm = ({handleSubmit, error, captchaUrl}) => {
     return (
         <form onSubmit={handleSubmit}>
             {createField("Email", "email", [required], Input)}
@@ -33,6 +33,9 @@ const LoginForm = ({handleSubmit, error}) => {
             {/*           name={"rememberMe"}*/}
             {/*           type={"checkbox"}/>remember me*/}
             {/*</div>*/}
+            {captchaUrl && <img src={captchaUrl} alt="captcha"/>}
+            {captchaUrl && createField("Symbols from image", "captcha", [required], Input)}
+
             { error && <div className={styleForm.formSummaryError}>
                 {error}
             </div>
@@ -48,7 +51,7 @@ const LoginReduxForm = reduxForm({form: 'login'})(LoginForm);
 
 const Login = (props) => {
     const onSubmit = (formData) => {
-        props.login(formData.email, formData.password, formData.rememberMe)
+        props.login(formData.email, formData.password, formData.rememberMe, formData.captcha)
     }
 
     if (props.isAuth) {
@@ -58,7 +61,7 @@ const Login = (props) => {
     return (
         <div className={style.login}>
             <h1>Login</h1>
-            <LoginReduxForm onSubmit={onSubmit} />
+            <LoginReduxForm onSubmit={onSubmit} captchaUrl={props.captchaUrl} />
             <div className={style.testAccData}>
                 <div>Test account data:</div>
                 <div>Email: free@samuraijs.com</div>
@@ -69,6 +72,7 @@ const Login = (props) => {
 }
 
 const mapStateToProps = (state) => ({
+    captchaUrl: state.auth.captchaUrl,
     isAuth: state.auth.isAuth,
 })
 
