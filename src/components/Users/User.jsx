56a8f04b52ba4f605c141b0bc4be165c59572c58
Khmara Dmytro <khmaracoach@gmail.com>
2021-03-08 00:49:43 +0200

    refactoring

--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -8,37 +8,30 @@ const User = ({user, followingInProgress, unfollow, follow}) => {
     return (
         <div className={styles.userItem}>
             <span>
-                <div>
+                <div className={styles.usersPhotoWrapper}>
                     <NavLink to={'/profile/' + user.id}>
                     <img src={user.photos.small != null ? user.photos.small : userPhoto} alt="photo"
                          className={styles.usersPhoto}/>
                     </NavLink>
                 </div>
-                {user.followed
-                    ? <button disabled={followingInProgress
-                        .some(id => id === user.id)}
-                              onClick={() => {
-                                  unfollow(user.id)
-                              }}>
-                        Unfollow</button>
-
-                    : <button disabled={followingInProgress
-                        .some(id => id === user.id)}
-                              onClick={() => {
-                                  follow(user.id)
-                              }}>
-                        Follow</button>}
-                    </span>
-            <span>
-                <span>
-                    <div>{user.name}</div>
-                    <div>{user.status}</div>
-                </span>
-                <span>
-                    <div>{"user.location.country"}</div>
-                    <div>{"user.location.city"}</div>
-                </span>
             </span>
+            <span>
+                <div className={styles.userName}>{user.name}</div>
+            </span>
+            {user.followed
+                ? <button disabled={followingInProgress
+                    .some(id => id === user.id)}
+                          onClick={() => {
+                              unfollow(user.id)
+                          }}>
+                    Unfollow</button>
+
+                : <button disabled={followingInProgress
+                    .some(id => id === user.id)}
+                          onClick={() => {
+                              follow(user.id)
+                          }}>
+                    Follow</button>}
         </div>
     );
 }
