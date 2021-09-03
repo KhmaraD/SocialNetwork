1f3ab1e601e22cdccfadccdbf4394b9bcf3dd3ec
Khmara Dmytro <khmaracoach@gmail.com>
2021-09-03 14:16:21 +0300

    test

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {Provider} from "react-redux";
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 ReactDOM.render(
     <BrowserRouter >
