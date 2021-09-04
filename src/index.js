649f17a676770eacf94b2575f36826a2d6c8c9d7
Khmara Dmytro <khmaracoach@gmail.com>
2021-09-04 14:51:51 +0300

    test

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {Provider} from "react-redux";
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
-    <BrowserRouter >
+    <HashRouter >
         <Provider store={store}>
             <App />
         </Provider>
-    </BrowserRouter>, document.getElementById('root'));
+    </HashRouter>, document.getElementById('root'));
 
 // HashRouter for correct display on the github
 
