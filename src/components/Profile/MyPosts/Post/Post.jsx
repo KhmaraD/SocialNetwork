865056eafb8c9d18a2fc975f9ecf26ce05ebf162
Khmara Dmytro <khmaracoach@gmail.com>
2021-09-04 15:13:24 +0300

    test

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import s from './Post.module.css';
+import postAvatar from '../../../../assets/images/avatar_post.jpg'
 
 const Post = (props) => {
   return (
     <div className={s.item}>
-      <img src="https://static.mk.ru/upload/entities/2019/05/08/00/articles/detailPicture/c7/b5/08/6e/5dda626cb409b1fa6942c29040609e17.jpg" />
+      <img src={postAvatar} alt="avatar_img"/>
       { props.message }
       <div>
         <span>Like</span> { props.likesCount }
